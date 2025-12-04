e9053fe31b654ebc7fa5da0c3e72798246a281f8
Ashot-js <ashotg771@gmail.com>
2025-12-04 21:26:09 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 
-import './App.css'
+import { useEffect, useState } from 'react'
+import './App.scss'
+import type { DataItem } from './types/types';
 
 function App() {
-
+  const [data, setData] = useState<DataItem[]>([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    .then(res=>res.json())
+    .then(data=>setData(data))
+    .catch(err=>console.log(err)
+    )
+  },[])
 
   return (
-    <>
-
-    </>
+    
+   <div className='App' >
+      {
+        data.map((elem)=>{
+        return(
+          <h1 key={elem.id}>{elem.title}</h1>
+        )
+        })
+      }
+   </div>
+    
   )
 }
 
---
+++ src/types/types.d.ts
@@ -0,0 +1,6 @@
+   export interface DataItem {
+   userId:number,
+   title:string,
+   body:string,
+   id:number
+ }
